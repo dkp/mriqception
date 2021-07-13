aa4dc88cb79715c422ecb770c502a877e03e6e3d
Dianne Patterson <dkp@arizona.edu>
2021-07-13 15:20:59 -0700

    Start to fool with colormaps in matplotlib.

--- tools/norm_z.py
+++ tools/norm_z.py
@@ -5,9 +5,13 @@
 # representation in a traffic-light table.
 
 # import os
+from matplotlib import cm
+from matplotlib import pyplot as plt
+
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
+
 # import plotly.graph_objects as go
 
 
@@ -16,7 +20,7 @@ import scipy.stats as stats
 # Argument is: 1) QA file
 # Example:  norm_z.py group_T1w.tsv
 # qa_file = sys.argv[1]
-qa_file = "../../data/group_bold.tsv"
+qa_file = "../../data/gtest.tsv"
 # print("qa_file is", qa_file)
 
 qa_df = pd.read_csv(qa_file, sep="\t")
@@ -35,3 +39,15 @@ bids_names = qa_df['bids_name']
 norm_df = pd.concat([bids_names, z_df], axis=1)
 print(norm_df)
 # print(norm_df[['aor', 'dummy_trs', 'fber', 'fd_num']])
+
+# create our own limited color map
+radish9 = cm.get_cmap('PiYG', 9)
+
+# set cell backgrounds based on data and our colormap
+# Note: unfortunately can't set discrete intervals or
+# center the colormap on zero.
+# norm_df.style.background_gradient(cmap=radish9, axis=None)
+
+# plt.imshow([[0, 1, 2, 3, 4, 5, 6, 7, 8]], cmap=radish9)
+plt.imshow(z_df, cmap=radish9)
+plt.show()
